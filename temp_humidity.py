924e35de20a58e3c92a137dbcba6ec5c06356ac0
Normand Cyr <norm@normandcyr.com>
2020-05-28 16:46:53 -0400

    Enable arguments to specify behaviour

--- temp_humidity.py
+++ temp_humidity.py
@@ -7,18 +7,19 @@ and relative humidity from a DHT22 module.
 
 The data is printed to the standard output, saved to a CSV file and to InfluxDB.
 
-Requires a running InfluxDB instance.
+Needs a running InfluxDB instance to save to database.
 Vizualisation can be done with Grafana.
 """
 
 import csv
 import time
+import argparse
 
 import serial
 from influxdb import InfluxDBClient
 
 __author__ = "Normand Cyr"
-__copyright__ = "Copyright 2020"
+__copyright__ = "Copyright 2020, Normand Cyr"
 __license__ = "GPL"
 __version__ = "0.0.1"
 __maintainer__ = "Normand Cyr"
@@ -26,16 +27,18 @@ __email__ = "normand.cyr@umontreal.ca"
 __status__ = "Development"
 
 
-def record_data(serial_data, output_csv_file, db_name):
+def get_serial_data(serial_port, baud_rate):
 
+    serial_data = serial.Serial(serial_port, baud_rate)
+    serial_data.flushInput()
     serial_bytes = serial_data.readline()
     decoded_bytes = serial_bytes.decode("utf-8")
-    humidity = decoded_bytes.split(",", 1)[0].replace("\r", "").replace("\n", "")
-    temperature = decoded_bytes.split(",", 1)[1].replace("\r", "").replace("\n", "")
+    humidity = float(decoded_bytes.split(",", 1)[0].replace("\r", "").replace("\n", ""))
+    temperature = float(
+        decoded_bytes.split(",", 1)[1].replace("\r", "").replace("\n", "")
+    )
 
-    print_stdout(temperature, humidity)
-    save_to_csv(output_csv_file, temperature, humidity)
-    save_to_db(db_name, temperature, humidity)
+    return temperature, humidity
 
 
 def print_stdout(temperature, humidity):
@@ -43,14 +46,14 @@ def print_stdout(temperature, humidity):
     print("temperature: {}°C, relative humidity: {}%".format(temperature, humidity))
 
 
-def save_to_csv(output_csv_file, temperature, humidity):
+def save_to_csv(args, temperature, humidity):
 
-    with open(output_csv_file, "a") as f:
+    with open(args.csv, "a") as f:
         writer = csv.writer(f, delimiter=",")
         writer.writerow([time.time(), temperature, humidity])
 
 
-def save_to_db(db_name, temperature, humidity):
+def save_to_db(args, temperature, humidity):
 
     json_body = [
         {
@@ -60,30 +63,76 @@ def save_to_db(db_name, temperature, humidity):
         }
     ]
 
-    db_client = InfluxDBClient(host="localhost", port=8086)
-    db_client.switch_database(db_name)
+    db_client = InfluxDBClient(host=args.host, port=args.port)
+    db_client.create_database(args.db)
+    db_client.switch_database(args.db)
     db_client.write_points(json_body, protocol="json")
 
 
-def main():
+def parse_args():
+
+    parser = argparse.ArgumentParser(
+        description="Arguments to specify the behaviour of the script."
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        required=False,
+        default="localhost",
+        help="hostname of InfluxDB http API",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        required=False,
+        default=8086,
+        help="port of InfluxDB http API",
+    )
+    parser.add_argument(
+        "--db",
+        nargs="?",
+        const="temp_humidity",
+        type=str,
+        help="saves the data to InfluxDB",
+    )
+    parser.add_argument(
+        "--csv",
+        nargs="?",
+        const="data.csv",
+        type=str,
+        help="saves the data to a csv file",
+    )
+    parser.add_argument(
+        "--stdout", action="store_true", help="prints the data to stdout",
+    )
+
+    return parser.parse_args()
+
+
+def main(args):
 
     serial_port = "/dev/ttyACM1"
     baud_rate = 9600
-    output_csv_file = "data.csv"
-    db_name = "temp_humidity"
 
     while True:
         try:
-            serial_data = serial.Serial(serial_port, baud_rate)
-            serial_data.flushInput()
-            record_data(serial_data, output_csv_file, db_name)
+            temperature, humidity = get_serial_data(serial_port, baud_rate)
+
+            if args.db:
+                save_to_db(args, temperature, humidity)
+            if args.csv:
+                save_to_csv(args, temperature, humidity)
+            if args.stdout:
+                print_stdout(temperature, humidity)
 
         except serial.SerialException:
             print("Port {} is unavailable.".format(serial_port))
             break
 
         except IndexError:
-            print("The output format is incorrect. Please review your Arduino code.")
+            print(
+                "The output format from the serial data is incorrect. Please review your Arduino code."
+            )
             break
 
         except KeyboardInterrupt:
@@ -92,4 +141,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    args = parse_args()
+    main(args)
